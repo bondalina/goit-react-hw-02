6dd818d76337d73ea6da83be97841bc6a2cc02f9
bondalina <142501520+bondalina@users.noreply.github.com>
2024-04-04 11:53:53 +0300

    basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import Description from "./components/Description/Description";
+import Options from "./components/Options/Options";
+import Feedback from "./components/Feedback/Feedback";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Description />
+      <Options />
+      <Feedback />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,7 @@
+import css from "./Feedback.module.css";
+
+const Feedback = () => {
+  return <div>Feedback</div>;
+};
+
+export default Feedback;

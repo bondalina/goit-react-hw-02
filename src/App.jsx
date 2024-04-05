bb76e41ec508f5a94051309838b0ccbc9f2fbef6
bondalina <142501520+bondalina@users.noreply.github.com>
2024-04-05 18:37:47 +0300

    5

--- src/App.jsx
+++ src/App.jsx
@@ -17,10 +17,15 @@ const App = () => {
       [feedbackType]: values[feedbackType] + 1,
     });
   };
-  const totalFeedback = values.good + values.neutral + values.bad;
   const resetFeedback = () => {
     setValues({ good: 0, neutral: 0, bad: 0 });
   };
+
+  const { good, neutral, bad } = values;
+  const totalFeedback = values.good + values.neutral + values.bad;
+  const positiveFeedback =
+    totalFeedback > 0 ? Math.round((good / totalFeedback) * 100) : 0;
+
   return (
     <div>
       <Description />
@@ -28,8 +33,17 @@ const App = () => {
         updateFeedback={updateFeedback}
         totalFeedback={totalFeedback}
         resetFeedback={resetFeedback}
+        positiveFeedback={positiveFeedback}
       />
-      {totalFeedback > 0 ? <Feedback values={values} /> : <Notification />}
+      {totalFeedback > 0 ? (
+        <Feedback
+          values={values}
+          totalFeedback={totalFeedback}
+          positiveFeedback={positiveFeedback}
+        />
+      ) : (
+        <Notification />
+      )}
     </div>
   );
 };
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,12 +1,14 @@
 import css from "./Feedback.module.css";
 
-const Feedback = ({ values }) => {
+const Feedback = ({ values, totalFeedback, positiveFeedback }) => {
   return (
     <div>
       <ul>
         <li>Good: {values.good}</li>
         <li>Neutral: {values.neutral}</li>
         <li>Bad: {values.bad}</li>
+        <p>Total feedback: {totalFeedback}</p>
+        <p>Positive feedback percentage: {positiveFeedback}%</p>
       </ul>
     </div>
   );
